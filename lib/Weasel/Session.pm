a3fb309b57bb936cb3befd0b3dadf7e1e67ed8a0
Erik Huelsmann <ehuels@gmail.com>
2019-02-17 23:44:39 +0100

    Update CHANGES, copyright years and version numbers for Weasel and Session

--- lib/Weasel/Session.pm
+++ lib/Weasel/Session.pm
@@ -5,7 +5,7 @@ Weasel::Session - Connection to an encapsulated test driver
 
 =head1 VERSION
 
-0.06
+0.07
 
 =head1 SYNOPSIS
 
@@ -50,7 +50,7 @@ use Module::Runtime qw/ use_module /;;
 use Weasel::FindExpanders qw/ expand_finder_pattern /;
 use Weasel::WidgetHandlers qw| best_match_handler_class |;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 
 =head1 ATTRIBUTES
