98f88cf4ab5e80f81dc77b8d2e3f06edce598f44
Yves Lavoie <ylavoie@yveslavoie.com>
2021-06-09 17:26:47 -0400

    Fix package version

--- lib/Weasel.pm
+++ lib/Weasel.pm
@@ -5,7 +5,7 @@ Weasel - Perl's php/Mink-inspired abstracted web-driver framework
 
 =head1 VERSION
 
-0.27
+0.28
 
 =head1 SYNOPSIS
 
@@ -116,7 +116,7 @@ use warnings;
 use Moose;
 use namespace::autoclean;
 
-our $VERSION = '0.27';
+our $VERSION = '0.28';
 
 # From https://w3c.github.io/webdriver/webdriver-spec.html#keyboard-actions
 my %key_codes = (
